3e1831fcda61f11a2ed6d7312c80cceff004796b
Yunsu <pianoest@naver.com>
2025-04-23 10:06:32 +0900

    feat: 초기 프로젝트 셋팅

---
+++ src/main/java/kr/ssok/ssokopenbanking/SsokopenbankingApplication.java
@@ -0,0 +1,13 @@
+package kr.ssok.ssokopenbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsokopenbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsokopenbankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ssok/ssokopenbanking/SsokopenbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ssok.ssokopenbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsokopenbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
