5f7c6bb1ccf16b5a5e02114726eb7d42712d2a35
Yunsu <pianoest@naver.com>
2025-05-09 09:27:13 +0900

    fix: CMD 수정 (#25, SCRUM-151)

--- src/main/java/kr/ssok/ssokopenbanking/global/comm/CommunicationProtocol.java
+++ src/main/java/kr/ssok/ssokopenbanking/global/comm/CommunicationProtocol.java
@@ -4,11 +4,5 @@ public class CommunicationProtocol {
 
     public static final String REQUEST_DEPOSIT = "kr.ssok.kafka.messaging.request.deposit";
     public static final String REQUEST_WITHDRAW = "kr.ssok.kafka.messaging.request.withdraw";
-    public static final String SEND_TEST_MESSAGE = "kr.ssok.kafka.messaging.test.message";
-
-    public static final String VALIDATE_ACCOUNT = "kr.ssok.kafka.messaging.request.validate.account";
-    public static final String CHECK_DORMANT = "kr.ssok.kafka.messaging.request.check.dormant";
-    public static final String CHECK_BALANCE = "kr.ssok.kafka.messaging.request.check.balance";
-    public static final String COMPENSATE_DEPOSIT = "kr.ssok.kafka.messaging.request.compensate.deposit";
     public static final String REQUEST_COMPENSATE = "kr.ssok.kafka.messaging.request.compensate";
 }
